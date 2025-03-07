add34a176d09dd4ee7b671d2243f5ffc6ffefee5
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-03-07 23:18:10 +0600

    fist commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+// student-crud-project
+//gggZSprj8hhtDhTt
+
+
+
+const uri = "mongodb+srv://student-crud-project:gggZSprj8hhtDhTt@cluster0.6hlcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+const app =express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('students simple crude ')
+})
+
+app.listen(port, ()=>{
+    console.log(`student crud is running ${port}`)
+})
