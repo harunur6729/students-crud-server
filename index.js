6effaf1bd76b50d0e2183840f6af98f08bd8cd96
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-03-09 01:39:56 +0600

    post and get api added

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
+const app =express();
+app.use(cors());
+app.use(express.json());
+
 // student-crud-project
 //gggZSprj8hhtDhTt
 
@@ -24,20 +28,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const StudentsCollection = client.db("StudentsDB").collection("students");
+
+    app.get('/students', async(req, res) =>{
+      const cursor = StudentsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/students', async(req, res)=>{
+      const student = req.body;
+      console.log('new students', student);
+
+      const result = await StudentsCollection.insertOne(student);
+      res.send(result)
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-const app =express();
-app.use(cors());
-app.use(express.json());
+
 
 
 app.get('/', (req, res)=>{
